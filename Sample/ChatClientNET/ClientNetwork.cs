0a651ef77a4954d66832c7f56faf44a8636ce412
Choi HeungBae <jacking75@gmail.com>
2014-04-11 09:16:13 +0900

    ChatClient의 닷넷 버전 아직 미완성

---
+++ Sample/ChatClientNET/ClientNetwork.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace ChatClientNET
+{
+    public class ClientNetwork
+    {
+        public Socket Sock = null;   
+        public string LatestErrorMsg;
+        byte[] ReadBuffer = new byte[4096];
+
+
+
+        //소켓연결        
+        public bool Connect(string ip, int port)
+        {
+            try
+            {
+                IPAddress serverIP = IPAddress.Parse(ip);
+                int serverPort = port;
+
+                Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Sock.Connect(new IPEndPoint(serverIP, serverPort));
+
+                if (Sock == null || Sock.Connected == false)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                LatestErrorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public ArraySegment<byte> Receive()
+        {
+            try
+            {
+                var nRecv = Sock.Receive(ReadBuffer, 0, ReadBuffer.Length, SocketFlags.None);
+
+                if (nRecv == 0)
+                {
+                    return new ArraySegment<byte>();
+                }
+
+                return new ArraySegment<byte>(ReadBuffer, 0, nRecv);
+            }
+            catch (SocketException se)
+            {
+                LatestErrorMsg = se.Message;
+            }
+
+            return new ArraySegment<byte>();
+        }
+
+        //스트림에 쓰기
+        public void Send(byte[] sendData)
+        {
+            try
+            {
+                if (Sock != null && Sock.Connected) //연결상태 유무 확인
+                {
+                    Sock.Send(sendData, 0, sendData.Length, SocketFlags.None);
+                }
+                else
+                {
+                    LatestErrorMsg = "먼저 채팅서버에 접속하세요!";
+                }
+            }
+            catch (SocketException se)
+            {
+                LatestErrorMsg = se.Message;
+            }
+        }
+
+        //소켓과 스트림 닫기
+        public void Close()
+        {
+            Sock.Shutdown(SocketShutdown.Both);
+            Sock.Close();
+        }
+
+        public bool IsConnected() { return (Sock != null && Sock.Connected) ? true : false; }
+    }
+}
---
+++ Sample/ChatClientNET/PacketBufferManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClientNET
+{
+    public class PacketBufferManager
+    {
+        int BufferSize = 0;
+        int ReadPos = 0;
+        int WritePos = 0;
+
+        int HeaderSize = 0;
+        int MaxPacketSize = 0;
+        byte[] PacketData;
+        byte[] PacketDataTemp;
+
+        public bool Init(int size, int headerSize, int maxPacketSize)
+        {
+            if (size < (maxPacketSize * 2) || size < 1 || headerSize < 1 || maxPacketSize < 1)
+            {
+                return false;
+            }
+
+            BufferSize = size;
+            PacketData = new byte[size];
+            PacketDataTemp = new byte[size];
+            HeaderSize = headerSize;
+            MaxPacketSize = maxPacketSize;
+
+            return true;
+        }
+
+        public bool Write(byte[] data, int pos, int size)
+        {
+            if (data == null || (data.Length < (pos + size)))
+            {
+                return false;
+            }
+
+            var remainBufferSize = BufferSize - WritePos;
+
+            if (remainBufferSize < size)
+            {
+                return false;
+            }
+
+            Buffer.BlockCopy(data, pos, PacketData, WritePos, size);
+            WritePos += size;
+
+            if (NextFree() == false)
+            {
+                BufferRelocate();
+            }
+
+            return true;
+        }
+
+        public ArraySegment<byte> Read()
+        {
+            var enableReadSize = WritePos - ReadPos;
+
+            if (enableReadSize < HeaderSize)
+            {
+                return new ArraySegment<byte>();
+            }
+
+            var bodySize = BitConverter.ToInt32(PacketData, (ReadPos+8));
+            var packetDataSize = HeaderSize + bodySize;
+
+            if (enableReadSize < packetDataSize)
+            {
+                return new ArraySegment<byte>();
+            }
+
+            var completePacketData = new ArraySegment<byte>(PacketData, ReadPos, packetDataSize);
+            ReadPos += packetDataSize;
+
+            return completePacketData;
+        }
+
+        bool NextFree()
+        {
+            var enableWriteSize = BufferSize - WritePos;
+
+            if (enableWriteSize < MaxPacketSize)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        void BufferRelocate()
+        {
+            var enableReadSize = WritePos - ReadPos;
+
+            if (enableReadSize <= ReadPos)
+            {
+                var tempBuffer = new ArraySegment<byte>(PacketData, ReadPos, enableReadSize);
+                Buffer.BlockCopy(tempBuffer.Array, 0, PacketData, 0, tempBuffer.Count);
+            }
+            else
+            {
+                Buffer.BlockCopy(PacketData, ReadPos, PacketDataTemp, 0, enableReadSize);
+                Buffer.BlockCopy(PacketDataTemp, 0, PacketData, 0, enableReadSize);
+            }
+
+            ReadPos = 0;
+            WritePos = enableReadSize;
+        }
+    }
+}
