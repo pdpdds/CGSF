a48dcb67d58cbb4c58ae643ce702e17b06d04a88
Juhang Park <juhang3@daum.net>
2014-04-22 17:35:35 +0900

    missing ChatClient

---
+++ Sample/ChatClientAndroid/src/com/cgsf/chatclientandroid/ChatClient.java
@@ -0,0 +1,55 @@
+package com.cgsf.chatclientandroid;
+
+import net.alhem.jsockets.SocketHandler;
+import net.alhem.jsockets.StdLog;
+import net.alhem.jsockets.StdoutLog;
+
+import java.util.Queue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: darkx
+ * Date: 14. 4. 22
+ * Time: 오후 5:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChatClient implements Runnable {
+
+    ChatActivity activity = null;
+    Queue<String> queue = null;
+    String szIP;
+    int port;
+    boolean isStop = false;
+
+    void setStop()
+    {
+        isStop = true;
+    }
+
+    void ProcessChatMessage(String szMessage)
+    {
+        activity.appendChatMessage(szMessage);
+    }
+
+    public ChatClient(ChatActivity activity, Queue<String> queue, String szIP, int port)
+    {
+        this.activity = activity;
+        this.queue = queue;
+        this.szIP = szIP;
+        this.port = port;
+    }
+
+    public void run() {
+        StdLog log = new StdoutLog();
+        SocketHandler h = new SocketHandler(log);
+        ChatClientSocket chatSocket = new  ChatClientSocket(h);
+        chatSocket.Open( szIP, port );
+        h.Add( chatSocket );
+        h.Select(1, 0);
+
+        while (isStop == false) // forever
+        {
+            h.Select(1, 0);
+        }
+    }
+}
