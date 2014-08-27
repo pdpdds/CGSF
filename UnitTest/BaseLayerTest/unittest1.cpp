77061ad52ea830875ca40026154de18642cb2220
Choi HeungBae <jacking75@gmail.com>
2014-08-27 14:18:42 +0900

    BaseLayer와 EngineLayer 유닛테스트 프로젝트 추가.
    EngineLayer 유닛테스트 빌드를 위해서 EngineLayer의 x64 플랫폼의 디버그모드의 속성에서 추가 라이브러리 디렉토리 수정 liblfds.lib의 경우 아래처럼 되어 있어서 유닛테스트 프로젝트에서 디렉토리 위치가 안 맞음.
    $(SolutionDir)/../ThirdParty/liblfds/lib/x64/DEBUG
    그래서 아래를 추가해서 해결
    $(SolutionDir)/../../ThirdParty/liblfds/lib/x64/DEBUG

---
+++ UnitTest/BaseLayerTest/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BaseLayerTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 코드는 여기서 테스트합니다.
+		}
+
+	};
+}
---
+++ UnitTest/EngineLayerTest/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EngineLayerTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 코드는 여기서 테스트합니다.
+		}
+
+	};
+}
