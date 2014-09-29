0bdaac96ebbcd08aef823fe2f95ea7c784c4bd4c
Choi HeungBae <jacking75@gmail.com>
2014-09-29 14:02:51 +0900

    NLog 추가

---
+++ NETFramework/CGSFNETCommon/FileLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGSFNETCommon
+{
+    public class FileLogger
+    {
+        public static Logger Logger = LogManager.GetLogger(DateTime.Now.ToString("yyyyMMddHHmm"));
+
+
+        public static void Debug(string message)
+        {
+            Logger.Debug(message);
+        }
+
+        public static void Info(string message)
+        {
+            Logger.Info(message);
+        }
+
+        public static void Error(string message)
+        {
+            Logger.Error(message);
+        }
+    }
+}
