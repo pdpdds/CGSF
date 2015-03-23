11f7ca533f2e7907dc861d433e5d5ed3b15de428
Juhang Park <juhang3@daum.net>
2015-03-24 02:41:24 +0900

    SFServiceController 서비스 실행 관련 코드 제거(서비스 등록, 제거, 시작, 정지 기능만 남겨 둠) SevenGameServer 서비스 처리 관련 코드 추가

--- BaseLayer/SFServiceController.h
+++ BaseLayer/SFServiceController.h
@@ -10,27 +10,4 @@ public:
 	BOOL DeleteService( TCHAR* szServiceName);
 	BOOL StartService(TCHAR* szServiceName);
 	BOOL StopService(TCHAR* szServiceName);
-
-	BOOL ServiceEntry(TCHAR* szServiceName, LPTHREAD_START_ROUTINE ServiceStartEntry);
-
-	static void ServiceMain(DWORD argc, LPTSTR *argv); 
-	static void ServiceCtrlHandler(DWORD nControlCode);
-	static BOOL UpdateServiceStatus(DWORD dwCurrentState, DWORD dwWin32ExitCode,
-		DWORD dwServiceSpecificExitCode, DWORD dwCheckPoint,
-		DWORD dwWaitHint);
-	static BOOL StartServiceThread();
-	static DWORD ServiceExecutionThread(LPDWORD param);
-	static LPTHREAD_START_ROUTINE funcServiceMainEntry;
-	
-	static void KillService();
-
-	static BOOL nServiceRunning;
-	static HANDLE killServiceEvent;
-
-protected:
-
-private:
-	static SERVICE_STATUS_HANDLE nServiceStatusHandle; 
-	static DWORD nServiceCurrentStatus;
-	static HANDLE hServiceThread;
 };
--- Sample/SevenGameServer/SevenGameServer.cpp
+++ Sample/SevenGameServer/SevenGameServer.cpp
@@ -11,14 +11,51 @@
 #include "SFUtil.h"
 #include "SFBreakPad.h"
 
-SFServiceController g_serviceController;
-bool ControlService(int argc, _TCHAR** argv);
+SERVICE_STATUS        g_ServiceStatus = { 0 };
+SERVICE_STATUS_HANDLE g_StatusHandle = NULL;
+HANDLE                g_ServiceStopEvent = INVALID_HANDLE_VALUE;
 
-static DWORD StartSevenGameService(LPDWORD param)
+bool ControlService(int argc, _TCHAR** argv);
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv);
+VOID WINAPI ServiceCtrlHandler(DWORD);
+
+//서비스를 구현한 함수
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam);
+
+int _tmain(int argc, _TCHAR* argv[])
 {
+//예외 핸들러 등록
 	SFBreakPad exceptionHandler;
 	exceptionHandler.Install();
 
+//파라메터가 존재할 경우 서비스 등록 및 삭제 루틴을 실행한다.	
+	if (argc == 2)
+	{
+		ControlService(argc, argv);
+		return 0;
+	}
+
+#ifdef _DEBUG
+	ServiceWorkerThread(NULL);
+#else
+	//서비스 메인 등록
+	SERVICE_TABLE_ENTRY ServiceTable[] =
+	{
+		{ argv[0], (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+		{ NULL, NULL }
+	};
+
+	if (StartServiceCtrlDispatcher(ServiceTable) == FALSE)
+	{
+		return 0;
+	}
+#endif
+
+	return 0;
+}
+
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
+{
 	TCHAR szFilePath[MAX_PATH] = { 0, };
 	GetModuleFileName(NULL, szFilePath, MAX_PATH);
 	TCHAR* szPath = SFUtil::ExtractPathInfo(szFilePath, SFUtil::PATH_DIR);
@@ -39,36 +76,21 @@ static DWORD StartSevenGameService(LPDWORD param)
 #ifdef _DEBUG
 	getchar();
 #else
-	WaitForSingleObject(SFServiceController::killServiceEvent, INFINITE);
-#endif
-
-	return 0;
-}
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	SFEngine::GetInstance();
-
-	if (argc == 2)
+	while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
 	{
-		ControlService(argc, argv);
-		return 0;
+		Sleep(3000);
 	}
-
-#ifdef _DEBUG	
-	StartSevenGameService(NULL);
-	
-#else	
-	g_serviceController.ServiceEntry(argv[0], (LPTHREAD_START_ROUTINE)StartSevenGameService	);
-#endif		
+#endif
 
 	SFEngine::GetInstance()->ShutDown();
 
-	return 0;
+	return ERROR_SUCCESS;
 }
 
 bool ControlService(int argc, _TCHAR** argv)
 {
+	SFServiceController serviceController;
+
 	TCHAR szFilePath[MAX_PATH] = { 0, };
 	TCHAR szFileName[MAX_PATH] = { 0, };
 	GetModuleFileName(NULL, szFilePath, MAX_PATH);
@@ -80,15 +102,104 @@ bool ControlService(int argc, _TCHAR** argv)
 	{
 		if (_tcsicmp(argv[1], L"-i") == 0)
 		{
-			return g_serviceController.InstallService(szFileName, szFileName, szFilePath);
+			return serviceController.InstallService(szFileName, szFileName, szFilePath);
 		}
 		else if (_tcsicmp(argv[1], L"-u") == 0)
 		{
-			return  g_serviceController.DeleteService(szFileName);
+			return  serviceController.DeleteService(szFileName);
 		}
 
 		printf("usage : filename -i(-u)\n");
 	}
 
 	return false;
+}
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
+{
+	DWORD Status = E_FAIL;
+
+	g_StatusHandle = RegisterServiceCtrlHandler(argv[0], ServiceCtrlHandler);
+
+	if (g_StatusHandle == NULL)
+	{
+		goto EXIT;
+	}
+
+	// Tell the service controller we are starting
+	ZeroMemory(&g_ServiceStatus, sizeof (g_ServiceStatus));
+	g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwServiceSpecificExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+	// Create stop event to wait on later.
+	g_ServiceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (g_ServiceStopEvent == NULL)
+	{
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+		g_ServiceStatus.dwWin32ExitCode = GetLastError();
+		g_ServiceStatus.dwCheckPoint = 1;
+
+		SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+		goto EXIT;
+	}
+
+	// Tell the service controller we are started
+	g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+	g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+	// Start the thread that will perform the main task of the service
+	HANDLE hThread = CreateThread(NULL, 0, ServiceWorkerThread, NULL, 0, NULL);
+
+	// Wait until our worker thread exits effectively signaling that the service needs to stop
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(g_ServiceStopEvent);
+
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 3;
+
+	SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+EXIT:
+	return;
+}
+
+
+VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
+{
+	switch (CtrlCode)
+	{
+	case SERVICE_CONTROL_STOP:
+
+		if (g_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
+			break;
+
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+		g_ServiceStatus.dwWin32ExitCode = 0;
+		g_ServiceStatus.dwCheckPoint = 4;
+
+		SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+		SetEvent(g_ServiceStopEvent);
+
+		break;
+
+	default:
+		break;
+	}
 }
